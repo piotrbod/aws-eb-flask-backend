1cb6efe693f4c6302facdc69327b06b489bdabdd
piotrbod <peter.bod@gmail.com>
2020-09-24 16:53:13 +0100

    Update application.py

--- files/application.py
+++ files/application.py
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production application.
     application.debug = True
-    application.run(host="0.0.0.0")
+    application.run(host="0.0.0.0", port=80)
