5bc214a7d0b95deb741c64a9a5ea03d85899ce43
piotrbod <peter.bod@gmail.com>
2020-09-24 16:42:40 +0100

    Update application.py

--- files/application.py
+++ files/application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-# print a nice greeting.
+# Print a nice greeting
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
-# some bits of text for the page.
+# Some bits of text for the page
 header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
 instructions = '''
@@ -14,21 +14,21 @@ instructions = '''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
-# EB looks for an 'application' callable by default.
+# Elastic Beanstalk looks for an 'application' that is callable by default
 application = Flask(__name__)
 
-# add a rule for the index page.
+# Add a rule for the index page
 application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
+# Add a rule when the page is accessed with a name appended to the site
+# URL
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
-# run the app.
+# Run the application
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
+    # removed before deploying a production application.
     application.debug = True
-    application.run()
+    application.run(host="0.0.0.0")
